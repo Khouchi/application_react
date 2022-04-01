f1003d5779c8a12cc5b1fa07c8acce383a102ca7
Khouchi <95851166+Khouchi@users.noreply.github.com>
2022-04-01 11:30:42 +0200

    hi

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1
+            ><br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sunt, est ipsam dolore aliquid veritatis iure debitis nisi, deserunt temporibus distinctio cumque eos dolorum optio blanditiis? Ut iusto alias optio magnam quo corrupti a molestiae adipisci repudiandae, veniam facere consectetur voluptatum perferendis dolores nisi laborum molestias placeat? Ratione accusamus, optio consequuntur iure corrupti deserunt aliquam accusantium? Quam consectetur sit corrupti atque voluptas est, vitae vero, libero eaque doloremque cumque facilis quae officia, consequatur dignissimos. Autem nesciunt architecto ratione rem? Illum dolorem quibusdam blanditiis tempora in dolor error, corporis quae voluptates neque molestias sed fugiat at. Iste aperiam saepe nam! Ad.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, eaque libero. Perferendis, officia. Cum aliquam, esse facere eos error pariatur repellendus quis maxime ratione exercitationem vel. Odio soluta cumque tenetur iusto ex, voluptas ipsum earum consequuntur ipsa? Voluptatibus magnam odit atque saepe fugit quasi aperiam dolore reiciendis, quidem vel culpa, ex, animi neque architecto rem eaque perspiciatis! Necessitatibus doloremque praesentium, reprehenderit consequuntur repellat laudantium vitae veniam temporibus officia sunt a perferendis ipsa mollitia! Nemo, accusantium assumenda illum placeat, repellat aliquam enim repudiandae voluptatum totam eos quaerat ratione, ducimus quis natus maiores incidunt temporibus sunt odio eaque recusandae blanditiis consectetur dignissimos? Perspiciatis sapiente dolor voluptatibus dolorem adipisci consequuntur non totam nobis eum facere vitae iusto fuga illum est molestiae blanditiis repellendus, quisquam cupiditate repellat minus veniam eius! Perspiciatis vitae animi assumenda sequi nemo quaerat ipsam commodi, iure fugiat porro placeat magni. Ut iure corporis, vitae voluptas illo debitis, architecto dignissimos placeat qui enim recusandae fugiat rem pariatur maiores ea nostrum magni, consequuntur voluptates? Animi atque laudantium, odio dolorum minus at modi obcaecati incidunt ab ipsam sit perspiciatis illum harum ut repudiandae adipisci consequuntur tempore dolorem voluptatem illo. Debitis necessitatibus quasi, ea eveniet possimus illo rem laudantium dolore omnis assumenda nesciunt magnam.</p>
+        </div>
+    );
+};
+
+export default About;
